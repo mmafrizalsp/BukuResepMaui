53e99ef53ba188f59e7df848751afbe2994d14de
Muhammad Ferry Afrizal <ferryafrizal@outlook.co.id>
2023-11-13 21:15:27 +0800

    save project

---
+++ Resepku/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Resepku;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ Resepku/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Resepku;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
